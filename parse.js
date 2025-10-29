b97479ebe41fbf50fff82ae0923abc1dc9c2a3c7
Nic <nicmrandazzo@gmail.com>
2025-10-29 14:02:14 -0700

    added live server for change viewing, buttons work

--- parse.js
+++ parse.js
@@ -1,11 +1,38 @@
 var curResume = "";
-
 function loadResume(ref){
 	const resumeSec = document.getElementById("resume-structured");
-	const div = resumeSec.createElement("div");
-	const pdf1 = await pdfjsLib.getDocument(ref);
-	console.log(pdf1);
+	
+	const div = document.createElement("canvas");
+	div.id = "pdf-canvas";
+	resumeSec.appendChild(div);
+	const pdf1 = pdfjsLib.getDocument(ref);
+	pdf1.promise.then((pdf) => {
+		pdf.getPage(1).then((page) => {
+			var scale = 1.5;
+			var viewport = page.getViewport({ scale: scale, });
+			// Support HiDPI-screens.
+			var outputScale = window.devicePixelRatio || 1;
 
+			var canvas = document.getElementById('pdf-canvas');
+			var context = canvas.getContext('2d');
+
+			canvas.width = Math.floor(viewport.width * outputScale);
+			canvas.height = Math.floor(viewport.height * outputScale);
+			canvas.style.width = Math.floor(viewport.width) + "px";
+			canvas.style.height =  Math.floor(viewport.height) + "px";
+
+			var transform = outputScale !== 1
+			  ? [outputScale, 0, 0, outputScale, 0, 0]
+			  : null;
+
+			var renderContext = {
+			  canvasContext: context,
+			  transform: transform,
+			  viewport: viewport
+			};
+page.render(renderContext);
+		});
+	});
 
 }
 
@@ -21,4 +48,3 @@ function downloadResume(){
 
 
 }
-
